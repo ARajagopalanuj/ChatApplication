be3f88e7270d070ec3eb79d3fcb67dd2e4b284c1
Rajagopalan A <a.rajagopalan415@gmail.com>
2025-11-10 20:14:21 +0530

    api updated

--- js/script.js
+++ js/script.js
@@ -55,7 +55,7 @@ document.getElementById("joinBtn").addEventListener("click", () => {
   }
 
   // connect to backend
-  socket = new WebSocket("wss://a662788df7d1.ngrok-free.app");
+  socket = new WebSocket("ws://f3625949f118.ngrok-free.app");
 
   socket.onopen = () => {
     console.log("✅ Connected to chat server");
